90c20bb9fcf0d0de76fa9268981a03f75cb880b0
siczekkonrad <konradd.siczek@gmail.com>
2017-01-05 12:11:45 +0100

    14th & 15th example

---
+++ 15_Paramter_object_destructuring.js
@@ -0,0 +1,23 @@
+console.clear();
+
+function ajax({
+		type = "get",
+		url = requiredParameter("url"),
+		data = {},
+		success = requiredParameter("success"),
+		error = () => {},
+		isAsync = true } = {}) {
+		console.log(JSON.stringify({type, url, data, success, error, isAsync}, null, 2));
+}
+
+
+function reqiuredParameter(name) {
+	throw new Error(`Missing parameter "${name}"`)
+}
+
+try {
+	ajax({
+		url: "https://my.api.io",
+		success: () => {}
+	});
+} catch (e) { console.warn(e.message) }
